d98ebfcff546999b618cc8c9a0825aad25ef3428
guruprasadh <guruprasadhmariappan@gmail.com>
2024-02-21 16:02:54 +0530

    Task 2 and 3 for day3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const req = new XMLHttpRequest();
 req.addEventListener("load", reqListener);
-req.open("GET","https://api.github.com/users/GURhttps://restcountries.com/v3.1/allUPRASADHM2003/following{/other_user}");
+req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
 req.onload = function () {
     const object=(JSON.parse(this.response));
@@ -10,7 +10,7 @@ req.onload = function () {
     for (let i = 0; i < object.length; i++) {
         console.log(object[i].name.common);
         console.log(object[i].region.common);
-        console.log(object[i].sub-region.common);
+        console.log(object[i].su-region.common);
     }
 
 };
