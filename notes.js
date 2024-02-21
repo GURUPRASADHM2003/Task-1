aaeab0d6f8b76fbb33e0bd401d5eb1abb67379ab
guruprasadh <guruprasadhmariappan@gmail.com>
2024-02-21 16:03:47 +0530

    confirm

--- notes.js
+++ notes.js
@@ -2,7 +2,7 @@
 
 const req = new XMLHttpRequest();
 req.addEventListener("load", reqListener);
-req.open("GET","https://api.github.com/users/GURUPRASADHM2https://restcountries.com/v3.1/all003/following{/other_user}");
+req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
 req.onload = function () {
     console.log(this.response);
