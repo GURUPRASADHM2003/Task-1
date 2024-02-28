b67f5acca8bd35903baaec7b1f8a13c2f2084abd
guruprasadh <guruprasadhmariappan@gmail.com>
2024-02-28 15:53:28 +0530

    CONFIRM

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 const req = new XMLHttpRequest();
-req.addEventListener("load", reqListener);
+
 req.open("GET", "https://restcountries.com/v3.1/all");
 req.send();
 req.onload = function () {
-    const object=(JSON.parse(this.response));
-    for (let i = 0; i < object.length; i++) {
-        console.log(object[i].flag.png);
-    }
-    for (let i = 0; i < object.length; i++) {
-        console.log(object[i].name.common);
-        console.log(object[i].region.common);
-        console.log(object[i].su-region.common);
-    }
-
+  const object = JSON.parse(this.response);
+  for (let i = 0; i < object.length; i++) {
+    console.log(object[i].flag.png);
+  }
 };
